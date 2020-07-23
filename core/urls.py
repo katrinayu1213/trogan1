ed8cdd65d32ad60eb846de7de56a3f16c9469f10
Katrina Yu <katrina.l.yu.21@dartmouth.edu>
2020-07-22 23:25:21 -0400

    created buttons for new entry, updated header text, updated password settings

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,8 @@ from core import views as core_views
 urlpatterns = [
         url(r'^$', auth_views.LoginView.as_view(template_name='landing.html'), name='landing'),
         url(r'^signup/$', core_views.signup, name='signup'),
-        url(r'^logout/$', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
+        # {'next_page': '/'}
+        url(r'^logout/$', auth_views.LogoutView.as_view(template_name='landing.html'), name='logout'),
         url(r'^home/$', login_required(core_views.HomePatientListView.as_view()), name='home'),
         url(r'^demographics/$', login_required(core_views.demographics), name='demographics'),
         url(r'^demographics/post_demogs/$', login_required(core_views.post_demogs), name='post_demogs'),
